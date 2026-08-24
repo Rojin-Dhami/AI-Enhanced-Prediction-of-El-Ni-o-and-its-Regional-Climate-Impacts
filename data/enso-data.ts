1fe92ef85ca0b0263a5da4164ef212a605902c80
LibraAkaja <adhikaribiraj908@gmail.com>
2026-08-24 21:35:15 +0545

    Add prototype data

---
+++ data/enso-data.ts
@@ -0,0 +1,62 @@
+export const ensoData = [
+  {
+    date: "Jan 2025",
+    observed: -0.4,
+    predicted: null,
+  },
+  {
+    date: "Feb 2025",
+    observed: -0.3,
+    predicted: null,
+  },
+  {
+    date: "Mar 2025",
+    observed: -0.1,
+    predicted: null,
+  },
+  {
+    date: "Apr 2025",
+    observed: 0.1,
+    predicted: null,
+  },
+  {
+    date: "May 2025",
+    observed: 0.2,
+    predicted: null,
+  },
+  {
+    date: "Jun 2025",
+    observed: 0.35,
+    predicted: 0.35,
+  },
+  {
+    date: "Jul 2025",
+    observed: null,
+    predicted: 0.48,
+  },
+  {
+    date: "Aug 2025",
+    observed: null,
+    predicted: 0.62,
+  },
+  {
+    date: "Sep 2025",
+    observed: null,
+    predicted: 0.78,
+  },
+  {
+    date: "Oct 2025",
+    observed: null,
+    predicted: 0.92,
+  },
+  {
+    date: "Nov 2025",
+    observed: null,
+    predicted: 1.05,
+  },
+  {
+    date: "Dec 2025",
+    observed: null,
+    predicted: 1.12,
+  },
+];
